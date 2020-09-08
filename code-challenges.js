c86399d4b9e27657959bfc7b93072972d82f0073
ryanizumi16 <ryanizumi16@gmail.com>
2020-09-07 22:54:49 -0700

    completeweek1assesment

--- code-challenges.js
+++ code-challenges.js
@@ -11,7 +11,18 @@ var temp1 = 35
 var temp2 = 350
 var temp3 = 212
 
-
+const checkTemp = (temp) => {
+  if(temp1 <= 212){
+    return `${temp1} is below boiling point`
+  }
+  else if(temp2 >= 212){
+    return `${temp2} is above boiling point`
+  }
+  else if(temp3 === 212){
+    return `${temp3} is at boiling point`
+  }
+}
+console.log(checkTemp(temp1))
 
 
 
@@ -20,6 +31,9 @@ var temp3 = 212
 
 var myNumbers1 = [3, 7, 0, 6, -9]
 
+  for(let i= 0; i < myNumbers1.length; i++) {
+    console.log(myNumbers1[i] * 5)
+  }
 
 
 
@@ -29,8 +43,10 @@ var myNumbers1 = [3, 7, 0, 6, -9]
 
 var myNumbers2 = [8, -7, 0, 6, 2]
 
-
-
+const mult5 = (array) => {
+  return array.map(value => value * 5)
+}
+console.log(mult5(myNumbers2))
 
 
 // --------------------4) Create a function that removes all the vowels from a string.
@@ -39,8 +55,15 @@ var myNumbers2 = [8, -7, 0, 6, 2]
 var stringWithVowels1 = "HeyThereLearnStudent"
 var stringWithVowels2 = "ILoveJavaScript"
 
-
-
+// const noVowels = (string) => {
+//   let newArray = string.split("")
+//   let filteredArray = newArray.filter(value => {
+//     return value !== "a" && value !== "e" && value !== "i" && value !== "o" && value !== "u"
+//   })
+//   let newString = filteredArray.join("")
+//   return newString
+// }
+// console.log(noVowels(stringWithVowels2))
 
 
 // --------------------5) Copy/paste your code from #4. Refactor your code to include non-string edge cases. Inform your user that the variable passed into the vowel removal function is not a string.
@@ -49,8 +72,21 @@ var stringWithVowels2 = "ILoveJavaScript"
 var notAString1 = true
 var notAString2 = 42
 
-
-
+const noVowels = (string) => {
+  // let newArray = string.split("")
+  // let filteredArray = newArray.filter(value => {
+  //   return value !== "a" && value !== "e" && value !== "i" && value !== "o" && value !== "u"
+  // })
+  // let newString = filteredArray.join("")
+  // return newString
+  if(true !== string){
+    return "true is not a string"
+  }
+  else if(42 !== string) {
+    return "42 is not a string"
+  }
+}
+console.log(noVowels(notAString1))
 
 
 // --------------------6) Create a function that takes the toonimals array and returns an array with only the toon objects that are cats.
@@ -58,9 +94,21 @@ var notAString2 = 42
 
 var toonimals = [ { name: "Itchy", animal: "mouse" }, { name: "Stimpy", animal: "cat" }, { name: "Daffy", animal: "duck" }, { name: "Scratchy", animal: "cat" }, { name: "Ren", animal: "dog" }, { name: "Felix", animal: "cat" }]
 
-
-
-
+var cats = toonimals.filter(value => {
+  if(value === "cat"){
+    return value.name
+  }
+})
+console.log(cats.name)
 
 // --------------------7) Using the toonimals variable from #6, create a function that returns only the names of the non-cats.
 // Expected output: "Itchy" "Daffy" "Ren"
+
+var toonimals = [ { name: "Itchy", animal: "mouse" }, { name: "Stimpy", animal: "cat" }, { name: "Daffy", animal: "duck" }, { name: "Scratchy", animal: "cat" }, { name: "Ren", animal: "dog" }, { name: "Felix", animal: "cat" }]
+
+var nonCats = toonimals.filter(value => {
+  if(value.name !== "cat"){
+    return value.name
+  }
+})
+console.log(nonCats.name)
